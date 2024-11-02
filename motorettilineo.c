9d5ac86f57d09744780200278c8ba872a28a0973
compleeee <125695064+compleeee@users.noreply.github.com>
2024-11-02 15:17:05 +0100

    Add files via upload

---
+++ motorettilineo.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+
+
+double main(){
+double s,t,t1,s1;
+printf("insert a value: ");
+scanf("%lf %lf %lf %lf", &s, &t, &s1, &t1);
+
+double v = s-s1/t-t1;
+
+
+printf("acceleration = %.2lf",v);
+}
