3175da45adba326bef06844f8bf8d23cb883228e
compleeee <125695064+compleeee@users.noreply.github.com>
2024-11-03 22:44:56 +0100

    Add files via upload
    da rivedere

---
+++ lamda.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+
+double main(){
+const double c = 3*10*10*10*10*10*10*10*10;
+
+
+double freq;
+
+
+printf("insert freq: ");
+scanf("%lf",&freq);
+double lamda = c/freq;
+printf("lamda = %.2lf" ,lamda);
+ 
+ 
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
