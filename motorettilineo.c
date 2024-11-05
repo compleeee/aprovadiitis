c534c50ea754a1c0a15ee09c1cd618dd9a3bfe46
compleeee <125695064+compleeee@users.noreply.github.com>
2024-11-05 19:55:34 +0100

    Update motorettilineo.c
    aggiustato il codice

--- motorettilineo.c
+++ motorettilineo.c
@@ -3,12 +3,15 @@
 
 
 double main(){
-double s,t,t1,s1;
-printf("insert a value: ");
-scanf("%lf %lf %lf %lf", &s, &t, &s1, &t1);
+  double s,t,t1,s1;
+  printf("insert a value: ");
+  scanf("%lf %lf %lf %lf", &s, &t, &s1, &t1);
 
-double v = s-s1/t-t1;
+  double v = s-s1/t-t1;
 
 
-printf("acceleration = %.2lf",v);
+  printf("acceleration = %.2lf",v);
+
+  return main();
+  return 0;
 }
