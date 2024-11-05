8fc2aa5a8999b94537deda393ffe6313e7c98ce5
compleeee <125695064+compleeee@users.noreply.github.com>
2024-11-05 20:27:05 +0100

    da finire

---
+++ isoterma.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "math.h"
+
+double main(){
+
+}
+
+double boyle(){
+    double p,p1,v,v1;
+    printf("insert a value");
+    scanf("%lf %lf %lf %lf", &p,&v,&p1,&v1);
+    if(v1=0){
+        v1=p*v/p1
+        printf("volume = %.2lf",v1);
+    }
+    else if(p=0){
+        p=p1*v2/v 
+        printf("pression = %.2lf",p)   
+
+    }
+}
