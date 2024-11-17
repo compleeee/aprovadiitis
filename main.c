c49e1da5f7bd3ca4c074574f30d419a37efbdcae
compleeee <125695064+compleeee@users.noreply.github.com>
2024-11-17 13:55:00 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double main()
+{
+    double a,b;
+    printf("inserisci i valori");
+    scanf("%lf %lf",&a &b);
+    double area = a*b;
+    printf("area = %2.lf",area);
+
+    return 0;
+
+}
